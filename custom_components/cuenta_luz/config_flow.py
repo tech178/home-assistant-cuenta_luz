f6d721a978d62e8accd17760d2a6b24d326fa939
tech178 <marcoa.henriquez@gmail.com>
2025-08-22 00:52:07 -0400

    Update config_flow.py

--- custom_components/cuenta_luz/config_flow.py
+++ custom_components/cuenta_luz/config_flow.py
@@ -6,10 +6,12 @@ class CuentaLuzConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        if user_input:
+        if user_input is not None:
             return self.async_create_entry(title=user_input["comuna"], data=user_input)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required("comuna"): str}),
+            data_schema=vol.Schema({
+                vol.Required("comuna"): str
+            })
         )
