2f3003a452d63a9a0d24ecff42ce1f9900c80000
tech178 <marcoa.henriquez@gmail.com>
2025-08-22 00:52:27 -0400

    Update sensor.py

--- custom_components/cuenta_luz/sensor.py
+++ custom_components/cuenta_luz/sensor.py
@@ -2,7 +2,7 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
-from .test_cuenta_luz import obtener_tarifas  # tu código de Selenium
+from .test_cuenta_luz import obtener_tarifas  # tu código de Selenium o requests/BS4
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     comuna = entry.data["comuna"]
@@ -13,15 +13,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 
 class TarifaSensor(SensorEntity):
     def __init__(self, comuna, key, value):
+        self._comuna = comuna
+        self._key = key
         self._attr_name = f"Cuenta Luz {comuna} {key}"
         self._attr_unique_id = f"cuenta_luz_{comuna}_{key}".replace(" ", "_").lower()
         self._attr_native_value = value
         self._attr_icon = "mdi:flash"
 
-    @property
-    def native_value(self):
-        return self._attr_native_value
-
     async def async_update(self):
-        data = obtener_tarifas(self._attr_name.split(" ")[2])
-        self._attr_native_value = data.get(self._attr_name.split(" ", 3)[-1], self._attr_native_value)
+        data = obtener_tarifas(self._comuna)
+        self._attr_native_value = data.get(self._key, self._attr_native_value)
